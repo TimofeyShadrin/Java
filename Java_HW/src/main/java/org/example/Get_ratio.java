8e31b144ae5ee93f4db373186c296a9260534cc0
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-11 19:24:15 +0300

    Get ratios - fixed (endWith added)

--- Java_HW/src/main/java/org/example/Get_ratio.java
+++ Java_HW/src/main/java/org/example/Get_ratio.java
@@ -21,50 +21,42 @@ public class Get_ratio {
                 .replace("^", "")
                 .replace(" = 0", "")
                 .replace(" + ", " ")
+                .trim()
                 .split(" ");
         int[][] ratios = new int[temp.length][2];
         int i = 0;
         for (String word: temp)
         {
             String[] value = word.split("X");
-            System.out.println(Arrays.toString(value));
+            //System.out.println(Arrays.toString(value));
             try
             {
-                if (String.valueOf(value[0]).equals(""))
-                {
-                    ratios[i][0] = 1;
-                }
-                else ratios[i][0] = Integer.parseInt(value[0]);
-
-                if (String.valueOf(value[1]).equals(""))
-                {
-                    if(word.equals("X")) {
-                        ratios[i][1] = 1;
+                if (value.length == 2) {
+                    if (String.valueOf(value[0]).equals("")) {
+                        ratios[i][0] = 1;
+                    } else {
+                        ratios[i][0] = Integer.parseInt(value[0]);
                     }
-                    else {
-                        ratios[i][1] = 0;
-                    }
-                }
-                else ratios[i][1] = Integer.parseInt(value[1]);
-
-            }
-            catch (Exception ArrayIndexOutOfBoundsException)
-            {
-                if (word.equals("X") && value.length == 0)
-                {
-                    assert ratios[i] != null;
-                    ratios[i][0] = 1;
-                    ratios[i][1] = 1;
-                }
-                else if (word.equals("X") && value[0] != null){
-                    ratios[i][0] = Integer.parseInt(value[0]);
-                    ratios[i][1] = 1;
-                }
-                else if (word.equals("X")){
-                    ratios[i][0] = 1;
                     ratios[i][1] = Integer.parseInt(value[1]);
                 }
-                else ratios[i][1] = 0;
+                else if (value.length == 1) {
+                    System.out.println(word);
+                    if (word.endsWith("X")) {
+                        ratios[i][0] = Integer.parseInt(value[0]);
+                        ratios[i][1] = 1;
+                    } else {
+                        ratios[i][0] = Integer.parseInt(value[0]);
+                        ratios[i][1] = 0;
+                    }
+                } else if (value.length == 0) {
+                    ratios[i][0] = 1;
+                    ratios[i][1] = 1;
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                System.out.println(String.valueOf(Ex));
             }
             i++;
         }
