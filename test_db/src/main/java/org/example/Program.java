c2c53ff6da3df308be9265bb3f85101e0745162e
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-12 12:43:50 +0300

    JavaFX tested

---
+++ test_db/src/main/java/org/example/Program.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Program {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "6773")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
