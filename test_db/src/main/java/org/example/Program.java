9d1a89b780feb0128c84f8501ef4961090868983
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-20 19:29:35 +0300

    Files.walk tested

--- test_db/src/main/java/org/example/Program.java
+++ test_db/src/main/java/org/example/Program.java
@@ -1,7 +1,12 @@
 package org.example;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +14,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static java.nio.file.Files.walk;
+
 public class Program extends Application {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (Connection conn = DriverManager.getConnection(
                 "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "6773")) {
 
@@ -27,6 +34,9 @@ public class Program extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+       List<Path> paths = walk(Paths.get("/home/timofei/Downloads"))
+                    .filter(Files::isRegularFile).toList();
+        System.out.println(paths.get(0).getFileName());
         Application.launch(args);
     }
 
