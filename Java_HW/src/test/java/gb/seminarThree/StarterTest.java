da2af2f1bf678227e3a85d61eba464a04e24ac89
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-22 09:16:27 +0300

    test class in task 0f seminar 3 added

---
+++ Java_HW/src/test/java/gb/seminarThree/StarterTest.java
@@ -0,0 +1,21 @@
+package gb.seminarThree;
+
+import org.junit.jupiter.api.Test;
+
+public class StarterTest {
+    @Test
+    void removeNegativeValue() {
+    }
+
+    @Test
+    void getMin() {
+    }
+
+    @Test
+    void getMax() {
+    }
+
+    @Test
+    void getAverage() {
+    }
+}
