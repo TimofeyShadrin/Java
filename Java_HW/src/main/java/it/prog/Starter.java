bee354a40b1d9f1aae088d19b9d423dbe3c15638
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-29 12:41:44 +0300

    thread tested

---
+++ Java_HW/src/main/java/it/prog/Starter.java
@@ -0,0 +1,37 @@
+package it.prog;
+
+class Some implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print(i);
+        }
+    }
+}
+
+public class Starter {
+    public static void main(String[] args) {
+        Thread newOne = new Thread (new Some());
+        Thread newTwo = new Thread (new Some());
+        newOne.start();
+        newTwo.start();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++){
+                try {
+                    Thread.sleep(750);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.print(i);
+            }
+        });
+        t1.start();
+    }
+}
