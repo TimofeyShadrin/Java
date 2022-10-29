830038079207061768bdb3b95c79daf9743625be
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-29 13:49:00 +0300

    thread tested static volatile

--- Java_HW/src/main/java/it/prog/Starter.java
+++ Java_HW/src/main/java/it/prog/Starter.java
@@ -1,39 +1,37 @@
 package it.prog;
 
 class Some implements Runnable {
-    public static int temp = 0;
+    public static volatile int temp = 0;
+
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++){
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        for (int i = 0; i < 10_000_000; i++){
             temp++;
-            System.out.println(temp);
+            //System.out.println(name + temp);
         }
     }
 }
 
 public class Starter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread newOne = new Thread (new Some());
         Thread newTwo = new Thread (new Some());
         newOne.start();
         newTwo.start();
 
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 10; i++){
-                try {
-                    Thread.sleep(750);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            for (int i = 0; i < 10_000_000; i++){
                 Some.temp++;
-                System.out.print(Some.temp);
+                //System.out.println("main: " + Some.temp);
             }
         });
         t1.start();
+
+        newOne.join();
+        newTwo.join();
+        t1.join();
+
+        System.out.println(Some.temp);
+
     }
 }
