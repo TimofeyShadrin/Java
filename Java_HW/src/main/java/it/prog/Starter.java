2d4c163f104de9d30f49e51e368a10adb0645bd1
Timofei Shadrin <tshadrin@yandex.ru>
2022-10-29 14:56:17 +0300

    start thread with antonym class

--- Java_HW/src/main/java/it/prog/Starter.java
+++ Java_HW/src/main/java/it/prog/Starter.java
@@ -1,31 +1,16 @@
 package it.prog;
 
-class Some implements Runnable {
-    public static int temp = 0;
-    public static final int N = 10_000_000;
-
-    public synchronized static void inc(){
-        temp++;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < N; i++){
-            inc();
-        }
-    }
-}
-
 public class Starter {
-    public static void main(String[] args) throws InterruptedException {
-        Thread newOne = new Thread (new Some());
-        Thread newTwo = new Thread (new Some());
-        newOne.start();
-        newTwo.start();
-
-        newOne.join();
-        newTwo.join();
-
-        System.out.println(Some.temp);
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName());
+            }).start();
+        }
     }
 }
